df6050a31cd84b197aa0cfd640ea49d9a4a1b1c5
tuan.nguyen <tuanchanged@gmail.com>
2019-03-31 22:19:28 +0700

    Testing_Conflict.py

---
+++ Testing_Conflict.py
@@ -0,0 +1,18 @@
+ from datetime import datetime
+ import xlsxwriter
+
+ # Create a workbook and add a worksheet.
+ workbook = xlsxwriter.Workbook('Expenses03.xlsx')
+ worksheet = workbook.add_worksheet()
+
+ # Add a bold format to use to highlight cells.
+ bold = workbook.add_format({'bold': 1})
+
+ # Add a number format for cells with money.
+ money_format = workbook.add_format({'num_format': '$#,##0'})
+
+ # Add an Excel date format.
+ date_format = workbook.add_format({'num_format': 'mmmm d yyyy'})
+
+ # Adjust the column width.
+ worksheet.set_column(1, 1, 15)
